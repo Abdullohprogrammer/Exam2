7fc9f473a35dbdad28787ebcea4084d51220ca28
abdulloh <abdullohprogrammer@gmail.com>
2023-07-24 14:04:30 +0500

    finnished

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$('.owl-carousel').owlCarousel({
+   loop:true,
+   margin:10,
+   nav:true,
+   dots: false,
+   responsive:{
+       0:{
+           items:1
+       },
+       600:{
+           items:3
+       },
+       1000:{
+           items:4
+       }
+   }
+})
+
+html
+$('.owl-carousel2').owlCarousel2({
+   loop:true,
+   margin:10,
+   nav:true,
+   dots: false,
+   responsive:{
+       0:{
+           items:1
+       },
+       600:{
+           items:2
+       },
+       1000:{
+           items:3
+       }
+   }
+})
+html
